cda98c113991cd0c0b992592815d0c6f797afd7b
RemyMz <remy.mazingue@gmail.com>
2025-11-07 11:29:50 +0100

    Initial setup: Maven project 'imgcompare' created

---
+++ imgcompare/src/main/java/fr/imt/raytracer/App.java
@@ -0,0 +1,13 @@
+package fr.imt.raytracer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
