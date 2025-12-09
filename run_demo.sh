4727c7cbbd3699f3b89d5e9eb73dbbe610f419d0
RemyMz <remy.mazingue@gmail.com>
2025-12-09 10:19:57 +0100

    Add final_avec_bonus.scene to the project

--- run_demo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo ">>> 1. Nettoyage et Compilation..."
-mvn clean package -DskipTests
-
-echo ">>> 2. Lancement du Rendu Final (Dragon)..."
-java -jar raytracer/target/raytracer-1.0-SNAPSHOT.jar scenes/final_avec_bonus.scene
-
-echo ">>> 3. Ouverture de l'image..."
-open output/dragon3.png || xdg-open output/dragon3.png
